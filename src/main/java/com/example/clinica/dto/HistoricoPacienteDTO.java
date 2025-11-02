760eaa3d0e98cf80e96f0107db253d3e1869458a
Adryan Masson <adryanpereiramasson@gmail.com>
2025-11-02 03:36:47 -0300

    DTO adicionado

---
+++ src/main/java/com/example/clinica/dto/HistoricoPacienteDTO.java
@@ -0,0 +1,101 @@
+package com.example.clinica.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class HistoricoPacienteDTO {
+
+    private Integer idConsulta;
+    private LocalDate dataConsulta;
+    private LocalTime horaInicio;
+    private LocalTime horaFim;
+    private String statusConsulta;
+    private Integer idProntuario;
+    private String anamnese;
+    private String diagnostico;
+    private String prescricao;
+    private String nomeMedico;
+
+    public HistoricoPacienteDTO() {
+    }
+
+    public Integer getIdConsulta() {
+        return idConsulta;
+    }
+
+    public void setIdConsulta(Integer idConsulta) {
+        this.idConsulta = idConsulta;
+    }
+
+    public LocalDate getDataConsulta() {
+        return dataConsulta;
+    }
+
+    public void setDataConsulta(LocalDate dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFim() {
+        return horaFim;
+    }
+
+    public void setHoraFim(LocalTime horaFim) {
+        this.horaFim = horaFim;
+    }
+
+    public String getStatusConsulta() {
+        return statusConsulta;
+    }
+
+    public void setStatusConsulta(String statusConsulta) {
+        this.statusConsulta = statusConsulta;
+    }
+
+    public Integer getIdProntuario() {
+        return idProntuario;
+    }
+
+    public void setIdProntuario(Integer idProntuario) {
+        this.idProntuario = idProntuario;
+    }
+
+    public String getAnamnese() {
+        return anamnese;
+    }
+
+    public void setAnamnese(String anamnese) {
+        this.anamnese = anamnese;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getPrescricao() {
+        return prescricao;
+    }
+
+    public void setPrescricao(String prescricao) {
+        this.prescricao = prescricao;
+    }
+
+    public String getNomeMedico() {
+        return nomeMedico;
+    }
+
+    public void setNomeMedico(String nomeMedico) {
+        this.nomeMedico = nomeMedico;
+    }
+}
